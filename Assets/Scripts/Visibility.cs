bb70a85229365e2c0a713c981fb71778076afbad
Phobez <abiaph@live.com>
2018-10-28 20:04:51 +0700

    Added Visibility UI elements

---
+++ Assets/Scripts/Visibility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Visibility : MonoBehaviour {
+
+    public Sprite visible;
+    public Sprite invisible;
+
+    private Hide player;
+
+    private Image img;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Hide>();
+
+        img = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.GetIsHiding())
+        {
+            img.sprite = invisible;
+        }
+        else
+        {
+            img.sprite = visible;
+        }
+	}
+}
