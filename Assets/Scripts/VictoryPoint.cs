8878f66f446df61d54ec0c7e5e2042a8be1e53f0
Phobez <abiaph@live.com>
2018-12-16 19:37:10 +0700

    Implemented victory point

---
+++ Assets/Scripts/VictoryPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryPoint : MonoBehaviour, IInteractable<GameObject> {
+
+	public void Interact(GameObject player)
+    {
+        GameManager.Instance.Victory(true);
+    }
+}
