f8b552f2b449f67d6d21ee0a809654fee7010e98
Phobez <abiaph@live.com>
2019-01-03 16:26:07 +0700

    Door locking system added
    - Added Locking and Unlocker components: Locking must be added to the locked object, unlocker must be added to the object which unlocks the locked object.
    - Added button UI sprite, Duty title sprite, and main menu background sprite
    - Added Fullscreen Sprite component to dynamically change background image's size

---
+++ Assets/Scripts/Unlocker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Made by : Abia Putrama Herlianto
+ *           3 January 2019
+ * SEE ALSO: Locking.cs
+ */
+
+// a component to creates an object that can unlock a designated locked object
+public class Unlocker : MonoBehaviour {
+
+    public enum UnlockerType { BUTTON, KEY };
+
+    public Locking lockedObject;
+    public UnlockerType unlockerType;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (unlockerType == UnlockerType.KEY)
+        {
+            lockedObject.SendMessage("Unlock");
+            Destroy(gameObject);
+        }
+        else if (unlockerType == UnlockerType.BUTTON)
+        {
+            lockedObject.SendMessage("Unlock");
+        }
+    }
+}
