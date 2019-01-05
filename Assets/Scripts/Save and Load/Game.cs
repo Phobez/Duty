d4b428a9e5d59b081c1ec40819ed321af12b39ec
Phobez <abiaph@live.com>
2019-01-05 21:58:58 +0700

    Added save and load
    - Created Game.cs: responsible for holding save data
    - Created SaveLoad.cs: responsible for actual saving and loading
    
    TO-DO: Implement it with the rest of the game

---
+++ Assets/Scripts/Save and Load/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Game : MonoBehaviour {
+
+    public static Game current;
+
+    public bool[] levels;
+
+    public Game()
+    {
+        levels = new bool[5];
+
+        levels[0] = true;
+
+        for (int i = 1; i < 5; i++)
+        {
+            levels[i] = false;
+        }
+    }
+}
