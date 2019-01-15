ad97f1d10ee30aab71262e4910074a63e0f9211b
Phobez <abiaph@live.com>
2019-01-15 19:27:48 +0700

    Fixed trap controller bug

---
+++ Assets/Scripts/Dialogue/DialogueActivator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueActivator : MonoBehaviour {
+
+    public GameObject dialogue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            dialogue.SetActive(true);
+        }
+    }
+}
