c79f2723156defecc3e0daf716b41c765d414470
Phobez <abiaph@live.com>
2019-01-15 21:52:58 +0700

    Fixed sleeping animation, created unlockable doors

---
+++ Assets/Scripts/Locking/DoorLockController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLockController : MonoBehaviour {
+
+    private Locking locking;
+    private SpriteRenderer sprRend;
+
+    public Sprite closed;
+    public Sprite open;
+
+	// Use this for initialization
+	void Start () {
+        locking = GetComponent<Locking>();
+        sprRend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (locking.locked)
+        {
+            sprRend.sprite = closed;
+        }
+        else
+        {
+            sprRend.sprite = open;
+        }
+	}
+}
